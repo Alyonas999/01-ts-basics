922db531e38f386926bcba809cd75bddfc937aaf
Alyonas999 <oranchuka@gmail.com>
2025-07-23 13:30:49 +0300

    1

--- src/task-1.ts
+++ src/task-1.ts
@@ -1,21 +1,7 @@
-interface Pers{
-    name: string;
-    age: number;
-    isOnline: boolean;
+const name: string = "Alice";
+const age: number = 30;
+const isOnline: boolean = true;
 
-}
-function pers(): void {
-    const name: string = "Alice";
-    const age: number = 30;
-    const isOnline: boolean = true;
-  
-    const person: Pers = {
-      name,
-      age,
-      isOnline,
-    };
 console.log(`Name: ${name}`);
 console.log(`Age: ${age}`);
 console.log(`Online: ${isOnline}`);
-}
-pers();
